dd4c57343c197dd4f8025c713468b352c61f1cf0
DevLeti <devleti0106@gmail.com>
2022-03-27 00:53:56 +0900

    ADD: JWT Authentication

--- file/models.py
+++ file/models.py
@@ -4,7 +4,7 @@ from folder.models import Folder
 
 
 class File(models.Model):
-    # upload_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    # folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True)
+    upload_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True)
     url = models.FileField(upload_to="")
-    # allowed_users = models.ManyToManyField(User, related_name="allowed_file", null=True)
+    allowed_users = models.ManyToManyField(User, related_name="allowed_file", null=True)
--- file/views.py
+++ file/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import FileUploadForm
 from .models import File
@@ -10,19 +14,24 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 JWT_authenticator = JWTAuthentication()
+@csrf_exempt
 def fileUpload(request):
     if request.method == 'POST':
-        # response = JWT_authenticator.authenticate(request)
-        # print(response)
+        user_info = JWT_authenticator.authenticate(request)
+
+        user_id=user_info[1]['user_id']
+        user = User.objects.get(pk=user_id)
+
         url = request.FILES["uploadedFile"]
         fileupload = File(
+            upload_user=user,
             url=url,
         )
         fileupload.save()
-        return redirect('fileupload')
-    else:
-        fileuploadForm = FileUploadForm
-        context = {
-            'fileuploadForm': fileuploadForm
-        }
-        return render(request, 'fileupload.html', context)
+        return render(request, 'uploadsuccess.html')
+    # else:
+    #     fileuploadForm = FileUploadForm
+    #     context = {
+    #         'fileuploadForm': fileuploadForm
+    #     }
+    #     return render(request, 'fileupload.html', context)
